04beb3285301cfb9a00e829c6e764031aa8f0a50
vladian95 <kvankvan1995@gmail.com>
2023-07-14 19:14:02 +0300

    y: 51

--- src/components/AnimatePage.jsx
+++ src/components/AnimatePage.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 const animations = {
   initial: { opacity: 0, y: 100 },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 50 },
+  exit: { opacity: 0, y: 51 },
 };
 
 const AnimatePage = ({ children }) => {
